c7c7601a3e223486598144d1480b5a96e7d1fb89
TeuTeuf <antoine.teuf@gmail.com>
2020-04-05 22:16:41 +0200

    Updates

---
+++ precache-manifest.57cff101f7b24e415463c93b90f02ad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4239c99e01485abd8bd60671b2171a3",
+    "url": "/turnip-market-frt/index.html"
+  },
+  {
+    "revision": "68f55de23bd8c8f1e5fb",
+    "url": "/turnip-market-frt/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "485ce18465ea336a646b",
+    "url": "/turnip-market-frt/static/js/2.a5ae9122.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/turnip-market-frt/static/js/2.a5ae9122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f55de23bd8c8f1e5fb",
+    "url": "/turnip-market-frt/static/js/main.30c6da59.chunk.js"
+  },
+  {
+    "revision": "d7306a84e966f85020f9",
+    "url": "/turnip-market-frt/static/js/runtime-main.39c5e183.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/turnip-market-frt/static/media/logo.5d5d9eef.svg"
+  }
+]);
